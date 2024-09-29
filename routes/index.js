dad2821b89b0d5d364a66682a4793d3e885d7c32
Obiajulu-gif <okoyeemmanuel998@gmail.com>
2024-09-29 09:05:34 +0100

    task 2 done

--- routes/index.js
+++ routes/index.js
@@ -9,14 +9,8 @@ const router = express.Router();
 // the get Routes
 router.get("/status", AppController.getStatus);
 router.get("/stats", AppController.getStats);
-router.get("/connect", AuthController.getConnect);
-router.get("/disconnect", AuthController.getDisconnect);
-router.get("/users/me", UsersController.getMe);
-router.get("/files/:id", FilesController.getShow);
-router.get("/files", FilesController.getIndex);
-
+r
 // the post Routes
 router.post("/users", UsersController.postNew);
-router.post("/files", FilesController.postUpload);
 
 module.exports = router;
